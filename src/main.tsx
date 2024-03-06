e3503ae1541e511f1794669ce425f463693cf5ea
Diogo Gonçalves <goncalves345diogo@gmail.com>
2024-03-06 15:19:14 +0000

    Add eslint setup
    This commit will add rocketseat/eslint to this project and run a lint to all files

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,5 @@ import { routes } from '@/routes/routes'
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <RouterProvider router={routes} />
-    </React.StrictMode>
+    </React.StrictMode>,
 )
